3c7bdd36f854b4ae47dafc79a4875bfee4aa43a9
bagdeanji <114647865+bagdeanji@users.noreply.github.com>
2022-12-13 12:45:14 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -6,6 +6,6 @@ instances = ['i-0a46ab2f0eb547e22']
 ec2 = boto3.client('ec2', region_name=region)
 
 def lambda_handler(event, context):
-    ec2.stop_instances(InstanceIds=instances)
+    ec2.start_instances(InstanceIds=instances)
     print('stopped your instances: ' + str(instances))
     print('sucess')
